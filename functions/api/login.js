2103fc307954e01bd82261c5be4c52943766be04
chy66666677889911 <chy66666678@gmail.com>
2026-08-25 18:21:48 +0800

    login.js

--- functions/api/login.js
+++ functions/api/login.js
@@ -1,15 +1,12 @@
 export async function onRequestPost({ request, env }) {
   const { username, password } = await request.json();
-  if (!/^\d{4}$/.test(username))
-    return Response.json({ ok: false, msg: "账号必须为4位数字ID" });
-
   const user = await env.DB.prepare(
-    "SELECT id,username,nickname,password,points,is_admin FROM users WHERE username=?"
+    `SELECT * FROM users WHERE username = ?`
   ).bind(username).first();
 
   if (!user) return Response.json({ ok: false, msg: "账号不存在" });
-  if (user.password !== password)
-    return Response.json({ ok: false, msg: "密码错误" });
+  if (user.password !== password) return Response.json({ ok: false, msg: "密码错误" });
+  if (user.status === 0) return Response.json({ ok: false, msg: "账号已被封禁" });
 
   return Response.json({
     ok: true,
