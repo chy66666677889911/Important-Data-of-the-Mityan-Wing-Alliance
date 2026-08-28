ed9d056393478172bf9924f3c66c58e4e8c7a2ae
chy66666677889911 <chy66666678@gmail.com>
2026-08-28 19:12:21 +0800

    Delete functions directory

--- functions/api/rank.js
+++
@@ -1,6 +0,0 @@
-export async function onRequest(context) {
-  const { env } = context;
-  const { DB } = env;
-  const list = await DB.prepare(`SELECT nickname, points FROM users ORDER BY points DESC LIMIT 20`).all();
-  return Response.json(list.results || []);
-}
